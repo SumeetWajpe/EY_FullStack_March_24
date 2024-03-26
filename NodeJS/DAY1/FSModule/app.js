d538032d7ad2912ad9496946b88e65bb82356a14
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-03-26 13:13:54 +0530

    ✨ useful links

--- NodeJS/DAY1/FSModule/app.js
+++ NodeJS/DAY1/FSModule/app.js
@@ -1,9 +1,13 @@
 const fs = require("fs"); // built-in
-fs.readFile("Input.txt", function (err, dataFromTheFile) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(dataFromTheFile.toString());
-  }
-});
+// Non-blocking way
+// fs.readFile("Input.txt", function (err, dataFromTheFile) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(dataFromTheFile.toString());
+//   }
+// });
+//Blocking
+const dataFromFile = fs.readFileSync("Input.txt");
+console.log(dataFromFile.toString());
 console.log("Ended !");
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # EY_FullStack_March_24
+
+# Javascript Event Loop
+https://www.youtube.com/watch?v=8aGhZQkoFbQ
+
+# Node Event Loop
+https://nodejs.org/en/learn/asynchronous-work/event-loop-timers-and-nexttick
