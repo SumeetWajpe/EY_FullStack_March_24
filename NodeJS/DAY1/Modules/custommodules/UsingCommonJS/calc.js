3158422a84a711c61af2792ac329ec460eaf0729
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-03-26 12:39:35 +0530

    ✨ Hello Node | Npm Packages | Custom modlues (CommonJS&ES6)

---
+++ NodeJS/DAY1/Modules/custommodules/UsingCommonJS/calc.js
@@ -0,0 +1,3 @@
+const mathModule = require("./math.js");
+console.log(mathModule.Add(20, 30));
+console.log(mathModule.Product(20, 30));
---
+++ NodeJS/DAY1/Modules/custommodules/UsingES6Modules/calc.js
@@ -0,0 +1,3 @@
+import { Add, Multiply as Product } from "./math.js";
+console.log(Add(20, 30));
+console.log(Product(20, 30));
