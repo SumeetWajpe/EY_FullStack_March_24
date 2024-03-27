9d92d0e534ad3c4fdcf0d3df28bd54221147a9a9
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-03-27 11:05:50 +0530

    ✨ Creating Readable & Writable Streams

---
+++ NodeJS/DAY2/FSAsEventEmitter/app.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+
+const readableStream = fs.createReadStream("Input.txt", "utf-8");
+const writableStream = fs.createWriteStream("Output.txt", "utf-8");
+
+let dataToBeWritten = "";
+readableStream.on("data", chunk => {
+  console.log("\r\n >>>>>>>>>>>>>>>>>>> CHUNCK >>>>>>>>>>>>>>>>>>>>> \r\n");
+  dataToBeWritten += chunk;
+  //   console.log(chunk);
+});
+
+readableStream.on("end", () => {
+  writableStream.write(dataToBeWritten);
+  writableStream.end();
+});
+
+console.log("Ended !");
