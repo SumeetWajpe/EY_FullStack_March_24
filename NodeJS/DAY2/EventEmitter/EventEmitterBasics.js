e5833c618a7389107b3ce42494bc8e6a11800eaa
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-03-27 10:28:36 +0530

    ✨ Emitting Custom Events (emit)

--- NodeJS/DAY2/EventEmitter/EventEmitterBasics.js
+++ NodeJS/DAY2/EventEmitter/EventEmitterBasics.js
@@ -4,7 +4,17 @@ const EventEmitter = require("events");
 function GetCount(maxCount) {
   let e = new EventEmitter();
   process.nextTick(() => {
-    e.emit("data", 1);
+    e.emit("start");
+    let counter = 0;
+    let t = setInterval(() => {
+      if (counter < maxCount) {
+        e.emit("data", counter++);
+      }
+      if (counter == maxCount) {
+        e.emit("end", counter);
+        clearInterval(t);
+      }
+    }, 500);
   });
   // biz logic
   return e;
@@ -12,6 +22,12 @@ function GetCount(maxCount) {
 
 // subscriber
 let evtEmitter = GetCount(10);
+evtEmitter.on("start", function () {
+  console.log("Started !");
+});
 evtEmitter.on("data", function (count) {
   console.log("Data Emitted with " + count);
+});
+evtEmitter.on("end", function () {
+  console.log("Ended !");
 });
