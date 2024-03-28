67eaf05a62986e90a54ada6cef3328bce0dabd84
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-03-28 10:10:27 +0530

    ✨ Websockets (Client & Server Peer communication - emit from server)

---
+++ NodeJS/DAY3/Websockets/serverpeer.js
@@ -0,0 +1,25 @@
+const http = require("http");
+const socket = require("socket.io");
+const fs = require("fs");
+const hostname = "127.0.0.1";
+const port = 3000;
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    const readableStream = fs.createReadStream("clientpeer.html");
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    readableStream.pipe(res);
+  }
+});
+
+var io = socket(server);
+io.sockets.on("connection", skt => {
+  setInterval(() => {
+    let dataToBeSent = new Date();
+    skt.emit("msg_from_server_peer", dataToBeSent);
+  }, 2000);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running @ port ${port}`);
+});
--- README.md
+++ README.md
@@ -5,3 +5,6 @@ https://www.youtube.com/watch?v=8aGhZQkoFbQ
 
 # Node Event Loop
 https://nodejs.org/en/learn/asynchronous-work/event-loop-timers-and-nexttick
+
+# Codeshare URL
+https://codeshare.io/beeqqV
