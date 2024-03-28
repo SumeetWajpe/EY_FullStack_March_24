0822625b4a0caf3e39f7042613b09f8826488630
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-03-28 11:30:41 +0530

    ✨ Using express.static middleware

--- NodeJS/DAY3/ExpressBasics/src/app.js
+++ NodeJS/DAY3/ExpressBasics/src/app.js
@@ -2,10 +2,26 @@ const express = require("express");
 const app = express(); // app represents the application
 const port = 3000;
 
+app.use(express.static("src/static")); // add express.static as a middleware
+
 // register an endpoint for GET verb with path as /
 app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>");
+  //   res.send("<h1>Hello World!</h1>");
+  res.sendFile("Index.html", { root: __dirname });
 });
+
+app.get("/products", (req, res) => {
+  let products = [
+    { id: 1, title: "Laptop", price: 100000 },
+    { id: 2, title: "LED TV", price: 50000 },
+  ];
+  res.json(products);
+});
+
+// app.get("/style.css", (req, res) => {
+//   //   res.send("<h1>Hello World!</h1>");
+//   res.sendFile("style.css", { root: __dirname });
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
