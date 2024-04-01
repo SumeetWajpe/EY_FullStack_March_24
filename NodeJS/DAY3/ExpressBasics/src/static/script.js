8be1d6aec5f7cd6d80486d9c08494a8cf52195af
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-04-01 10:01:00 +0530

    ✨ Displaying Products as card in UI

--- NodeJS/DAY3/ExpressBasics/src/static/script.js
+++ NodeJS/DAY3/ExpressBasics/src/static/script.js
@@ -1,1 +1,30 @@
-console.log("Script loaded on client !");
+document.addEventListener("DOMContentLoaded", LoadProducts);
+
+async function LoadProducts() {
+  let res = await fetch("http://localhost:3000/products");
+  let products = await res.json();
+
+  let row = document.createElement("div");
+  row.className = "row g-2";
+  let main = document.getElementById("main");
+
+  for (const product of products) {
+    row.innerHTML += CreateProductCard(product);
+  }
+  main.append(row);
+}
+
+function CreateProductCard(product) {
+  let card = `<div class="col-md-3"><div class="card">
+  <img src=${product.imageUrl} class="card-img-top" alt=${product.title} width="200px" height="150px">
+  <div class="card-body">
+    <h5 class="card-title">${product.title}</h5>
+    <p class="card-text">₹. ${product.price}</p>
+    <p class="card-text"> ${product.rating}</p>
+    <button class="btn btn-primary"> ${product.likes}</button>
+  </div>
+</div>
+</div>
+`;
+  return card;
+}
