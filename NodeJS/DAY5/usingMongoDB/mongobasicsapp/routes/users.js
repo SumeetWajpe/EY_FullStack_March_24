8cd20e44ce99f7df0a82221c34a7153100875d48
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-04-03 10:28:31 +0530

    ✨ Fetching users from users collection in testdb

--- NodeJS/DAY5/usingMongoDB/mongobasicsapp/routes/users.js
+++ NodeJS/DAY5/usingMongoDB/mongobasicsapp/routes/users.js
@@ -1,9 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const User = require("../models/users.model");
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", async function (req, res) {
+  try {
+    //fetch all users
+    let users = await User.find({});
+    res.json({ users });
+  } catch (error) {}
 });
 
 module.exports = router;
