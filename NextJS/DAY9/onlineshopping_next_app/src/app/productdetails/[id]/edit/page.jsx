ed32f37b01a06884f5ff96105f247c8f00b55118
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-04-12 10:58:55 +0530

    ✨ Defining a basic Client Compoent (EditProductForm)

--- NextJS/DAY9/onlineshopping_next_app/src/app/productdetails/[id]/edit/page.jsx
+++ NextJS/DAY9/onlineshopping_next_app/src/app/productdetails/[id]/edit/page.jsx
@@ -1,8 +1,34 @@
+import EditProductForm from "@/components/client/EditProductForm";
 import React from "react";
 
-function EditPage(props) {
-  let id = props.params.id;
-  return <div>Editing Product with Id - {id}</div>;
+async function getProduct(id) {
+  const res = await fetch("http://localhost:3100/products/" + id);
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
+}
+
+async function EditPage(props) {
+  let theProduct;
+  try {
+    theProduct = await getProduct(props.params.id);
+  } catch (error) {
+    if (!theProduct?.product) {
+      notFound();
+      //return <h1 className="text-danger"> Could not find the product !</h1>;
+    }
+  }
+  return (
+    <div>
+      Editing Product with Id - {theProduct.id} with title {theProduct.title}
+      <EditProductForm theProduct={theProduct} />
+    </div>
+  );
 }
 
 export default EditPage;
---
+++ NextJS/DAY9/onlineshopping_next_app/src/components/client/editproductform.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+function EditProductForm({ theProduct }) {
+  return (
+    <div>
+      <button onClick={() => console.log("U Clicked !")}>Click Me !</button>
+    </div>
+  );
+}
+
+export default EditProductForm;
