947267744b6233f3aecc0607b6c9405d2f014dc9
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-04-15 11:40:37 +0530

    ✨ Using React Hook Form for adding new product

--- NextJS/DAY9/onlineshopping_next_app/src/app/newproduct/page.jsx
+++ NextJS/DAY9/onlineshopping_next_app/src/app/newproduct/page.jsx
@@ -1,79 +1,93 @@
-import React from "react";
-import { redirect } from "next/navigation";
-async function addNewProductDataToServer(newProduct) {
-  const res = await fetch("http://localhost:3100/products/new", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(newProduct),
-  });
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
+// import React from "react";
+// import { redirect } from "next/navigation";
+// async function addNewProductDataToServer(newProduct) {
+//   const res = await fetch("http://localhost:3100/products/new", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(newProduct),
+//   });
+//   // The return value is *not* serialized
+//   // You can return Date, Map, Set, etc.
 
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
-  }
+//   if (!res.ok) {
+//     // This will activate the closest `error.js` Error Boundary
+//     throw new Error("Failed to fetch data");
+//   }
 
-  return res.json();
-}
+//   return res.json();
+// }
 
-function NewProduct() {
-  async function addNewProduct(formData) {
-    // Define a server action
-    "use server";
-    // Get User Input
-    const id = formData.get("pid");
-    const title = formData.get("ptitle");
-    const price = formData.get("pprice");
-    const likes = formData.get("plikes");
-    const rating = formData.get("prating");
-    const imageUrl = formData.get("pimageurl");
+// function NewProduct() {
+//   async function addNewProduct(formData) {
+//     // Define a server action
+//     "use server";
+//     // Get User Input
+//     const id = formData.get("pid");
+//     const title = formData.get("ptitle");
+//     const price = formData.get("pprice");
+//     const likes = formData.get("plikes");
+//     const rating = formData.get("prating");
+//     const imageUrl = formData.get("pimageurl");
 
-    let newProduct = { id, title, price, likes, rating, imageUrl };
+//     let newProduct = { id, title, price, likes, rating, imageUrl };
 
-    // Add New Product (fetch)
-    let message = await addNewProductDataToServer(newProduct);
-    if (message.msg) {
-      console.log(message.msg); // update a label
-      redirect("/products");
-    }
-    // Redirect to /products
-  }
+//     // Add New Product (fetch)
+//     let message = await addNewProductDataToServer(newProduct);
+//     if (message.msg) {
+//       console.log(message.msg); // update a label
+//       redirect("/products");
+//     }
+//     // Redirect to /products
+//   }
+//   return (
+//     <div>
+//       <form action={addNewProduct}>
+//         <div className="col-md-4">
+//           <div className="row">
+//             <label htmlFor="txtProductId">Id :</label>
+//             <input type="number" id="txtProductId" name="pid" />
+//           </div>
+//           <div className="row">
+//             <label htmlFor="txtProductTitle">Title :</label>
+//             <input type="text" id="txtProductTitle" name="ptitle" />
+//           </div>
+          // <div className="row">
+          //   <label htmlFor="txtProductPrice">Price :</label>
+          //   <input type="number" id="txtProductPrice" name="pprice" />
+          // </div>
+          // <div className="row">
+          //   <label htmlFor="txtProductRating">Rating :</label>
+          //   <input type="number" id="txtProductRating" name="prating" />
+          // </div>
+          // <div className="row">
+          //   <label htmlFor="txtProductLikes">Likes :</label>
+          //   <input type="number" id="txtProductLikes" name="plikes" />
+          // </div>
+          // <div className="row">
+          //   <label htmlFor="txtProductImageUrl">Image Url :</label>
+          //   <input type="text" id="txtProductImageUrl" name="pimageurl" />
+          // </div>
+//         </div>
+//         <button className="btn btn-success">Add New Product</button>
+//       </form>
+//     </div>
+//   );
+// }
+
+// export default NewProduct;
+
+
+import NewProductForm_ReactHookForm from '@/components/client/newproductform'
+import React from 'react'
+
+function NewProductUsingReactHookForm() {
   return (
     <div>
-      <form action={addNewProduct}>
-        <div className="col-md-4">
-          <div className="row">
-            <label htmlFor="txtProductId">Id :</label>
-            <input type="number" id="txtProductId" name="pid" />
-          </div>
-          <div className="row">
-            <label htmlFor="txtProductTitle">Title :</label>
-            <input type="text" id="txtProductTitle" name="ptitle" />
-          </div>
-          <div className="row">
-            <label htmlFor="txtProductPrice">Price :</label>
-            <input type="number" id="txtProductPrice" name="pprice" />
-          </div>
-          <div className="row">
-            <label htmlFor="txtProductRating">Rating :</label>
-            <input type="number" id="txtProductRating" name="prating" />
-          </div>
-          <div className="row">
-            <label htmlFor="txtProductLikes">Likes :</label>
-            <input type="number" id="txtProductLikes" name="plikes" />
-          </div>
-          <div className="row">
-            <label htmlFor="txtProductImageUrl">Image Url :</label>
-            <input type="text" id="txtProductImageUrl" name="pimageurl" />
-          </div>
-        </div>
-        <button className="btn btn-success">Add New Product</button>
-      </form>
+      <NewProductForm_ReactHookForm />
     </div>
-  );
+  )
 }
 
-export default NewProduct;
+export default NewProductUsingReactHookForm
---
+++ NextJS/DAY9/onlineshopping_next_app/src/components/client/newproductform.jsx
@@ -0,0 +1,59 @@
+"use client";
+import React from "react";
+import { useForm } from "react-hook-form";
+
+function NewProductForm_ReactHookForm() {
+  const { register, handleSubmit } = useForm();
+  return (
+    <div>
+      <form
+        onSubmit={handleSubmit(formData => {
+          console.log(formData);
+        })}
+      >
+        <div className="col-md-4">
+          <div className="row">
+            <label htmlFor="txtProductId">Id :</label>
+            <input type="number" id="txtProductId" {...register("id")} />
+          </div>
+          <div className="row">
+            <label htmlFor="txtProductTitle">Title :</label>
+            <input type="text" id="txtProductTitle" {...register("title")} />
+          </div>
+
+          <div className="row">
+            <label htmlFor="txtProductPrice">Price :</label>
+            <input type="number" id="txtProductPrice" {...register("price")} />
+          </div>
+          <div className="row">
+            <label htmlFor="txtProductRating">Rating :</label>
+            <input
+              type="number"
+              id="txtProductRating"
+              {...register("rating")}
+            />
+          </div>
+          <div className="row">
+            <label htmlFor="txtProductLikes">Likes :</label>
+            <input type="number" id="txtProductLikes" {...register("likes")} />
+          </div>
+          <div className="row">
+            <label htmlFor="txtProductImageUrl">Image Url :</label>
+            <input
+              type="text"
+              id="txtProductImageUrl"
+              {...register("imageUrl")}
+            />
+          </div>
+          <div className="row">
+            <label htmlFor="txtProductImageUrl">Description :</label>
+            <textarea id="txtProductImageUrl" {...register("description")} />
+          </div>
+          <button className="btn btn-success">Add New Product</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default NewProductForm_ReactHookForm;
