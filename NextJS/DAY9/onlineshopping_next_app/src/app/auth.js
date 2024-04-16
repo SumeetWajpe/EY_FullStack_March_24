103510087db9a6590397daab89da9af9ffd45eff
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-04-16 10:40:42 +0530

    ✨ Adding NextAuth | route.js | auth.js

---
+++ NextJS/DAY9/onlineshopping_next_app/src/app/auth.js
@@ -0,0 +1,29 @@
+import NextAuth from "next-auth";
+import credentials from "next-auth/providers/credentials";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const {
+  auth,
+  handlers: { GET, POST },
+} = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "User Credentials",
+      credentials: {
+        username: { type: String, required: true },
+        password: { type: String, required: true },
+      },
+      authorize: async credentials => {
+        // data fetch
+        const user = { id: 1, name: "sumeet", password: "admin" }; // db
+        if (
+          credentials?.username === user.name &&
+          credentials?.password === user.password
+        ) {
+          return user;
+        } else return null;
+      },
+    }),
+  ],
+  secret: process.env.AUTH_SECRET,
+});
