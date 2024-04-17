b82f08c234c8342e9026390871e890f5a9cd8380
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-04-17 12:08:21 +0530

    ✨ Simulating Click Event with Enzyme

--- Testing/unit-testing-app/src/counter.jsx
+++ Testing/unit-testing-app/src/counter.jsx
@@ -6,7 +6,7 @@ class Counter extends React.Component {
     return (
       <>
         <p>{this.state.count}</p>
-        <button onClick={this.setState({ count: this.state.count + 1 })}>
+        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
           ++
         </button>
       </>
--- Testing/unit-testing-app/src/counter.test.js
+++ Testing/unit-testing-app/src/counter.test.js
@@ -5,14 +5,23 @@ import Counter from "./counter";
 Enzyme.configure({ adapter: new Adapter() });
 
 describe("test suite for counter component", () => {
+  let componentInst;
+  beforeEach(() => {
+    componentInst = shallow(<Counter />);
+  });
   it("tests if counter component is instanciated", () => {
-    let componentInst = shallow(<Counter />);
     expect(componentInst).toBeDefined();
   });
 
-  it("checks if state in initialized to ten", () => {
-    let componentInst = shallow(<Counter />);
+  it("tests if state in initialized to ten", () => {
     let theCount = componentInst.state().count;
     expect(theCount).toBe(10);
+  });
+
+  fit("tests if value of state incremented on click of the button", () => {
+    let button = componentInst.find("button");
+    button.simulate("click");
+    let pText = componentInst.find("p").text();
+    expect(pText).toBe("11");
   });
 });
